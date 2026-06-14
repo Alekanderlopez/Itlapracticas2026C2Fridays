5db11ed6e2f78b80e52f4aca939d3f736d9c8575
Alexander lopez <alex.lopez3164@gmail.com>
2026-06-13 20:05:01 -0400

    Final project start

---
+++ MyPractices/MyfinalProject/MyfinalProject/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Alexander José López Almonte
+//Proyecto: Control de Vehículos de una Empresa
+//Descripción: Aplicación en consola para registrar vehículos, conductores y mantenimientos. Permite CRUD de vehículos y seguimiento de revisiones. Clases sugeridas: Vehiculo, Conductor, Mantenimiento.
+
+
+
+//Modelos
+class Vehiculo
+{
+    public string Placa { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+
+    public Vehiculo(string placa, string marca, string modelo)
+    {
+        Placa = placa;
+        Marca = marca;
+        Modelo = modelo;
+    }
