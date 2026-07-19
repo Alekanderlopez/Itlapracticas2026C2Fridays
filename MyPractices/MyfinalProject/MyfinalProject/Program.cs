240804700337d93dd0afad458b69095563c451a2
Alexander lopez <alex.lopez3164@gmail.com>
2026-07-18 20:05:28 -0400

    Update option
    Option

--- MyPractices/MyfinalProject/MyfinalProject/Program.cs
+++ MyPractices/MyfinalProject/MyfinalProject/Program.cs
@@ -2,13 +2,11 @@
 //Alexander José López Almonte
 //Proyecto: Control de Vehículos de una Empresa
 //Descripción: Aplicación en consola para registrar vehículos, conductores y mantenimientos. Permite CRUD de vehículos y seguimiento de revisiones. Clases sugeridas: Vehiculo, Conductor, Mantenimiento.
+using Microsoft.Data.SqlClient;
 
 
-
-static void Main()
+int option;
 {
-    int option;
-
     do
     {
         Console.WriteLine("\n VEHICLE CONTROL SYSTEM");
@@ -35,48 +33,4 @@ static void Main()
         }
 
     } while (option != 4);
-}
-
-class Vehicle
-{
-    public string LicensePlate { get; set; }
-    public string Brand { get; set; }
-    public string Model { get; set; }
-
-    public Vehicle(string licensePlate, string brand, string model)
-    {
-        LicensePlate = licensePlate;
-        Brand = brand;
-        Model = model;
-    }
-}
-
-class Driver
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string LicenseNumber { get; set; }
-
-    public Driver(int id, string name, string licenseNumber)
-    {
-        Id = id;
-        Name = name;
-        LicenseNumber = licenseNumber;
-    }
-}
-
-class Maintenance
-{
-    public int Id { get; set; }
-    public string VehicleLicensePlate { get; set; }
-    public DateTime Date { get; set; }
-    public string Description { get; set; }
-
-    public Maintenance(int id, string vehicleLicensePlate, DateTime date, string description)
-    {
-        Id = id;
-        VehicleLicensePlate = vehicleLicensePlate;
-        Date = date;
-        Description = description;
-    }
 }
