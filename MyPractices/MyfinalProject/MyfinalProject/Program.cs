8c9d330ba7a776a153c5aceb170b2db1536a1d28
Alexander lopez <alex.lopez3164@gmail.com>
2026-07-18 20:11:10 -0400

    Update
    Update

--- MyPractices/MyfinalProject/MyfinalProject/Program.cs
+++ MyPractices/MyfinalProject/MyfinalProject/Program.cs
@@ -5,6 +5,27 @@
 using Microsoft.Data.SqlClient;
 
 
+
+try
+{
+    string connectionString =
+        @"Server=localhost;Database=VehicleManagement;Trusted_Connection=True;TrustServerCertificate=True;";
+
+    SqlConnection connection = new SqlConnection(connectionString);
+
+    connection.Open();
+
+    Console.WriteLine("Connected successfully!");
+
+    connection.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
 int option;
 {
     do
@@ -34,3 +55,63 @@ int option;
 
     } while (option != 4);
 }
+
+
+
+class Vehicle
+{
+    public string LicensePlate { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+
+    public Vehicle(string licensePlate, string brand, string model)
+    {
+        LicensePlate = licensePlate;
+        Brand = brand;
+        Model = model;
+    }
+}
+
+class Driver
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LicenseNumber { get; set; }
+
+    public Driver(int id, string name, string licenseNumber)
+    {
+        Id = id;
+        Name = name;
+        LicenseNumber = licenseNumber;
+    }
+}
+
+class Maintenance
+{
+    public int Id { get; set; }
+    public string VehicleLicensePlate { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+
+    public Maintenance(int id, string vehicleLicensePlate, DateTime date, string description)
+    {
+        Id = id;
+        VehicleLicensePlate = vehicleLicensePlate;
+        Date = date;
+        Description = description;
+    }
+}
+
+
+
+public class DatabaseConnection
+{
+    private string connectionString =
+        @"Server=.\SQLEXPRESS;Database=VehicleManagement;Trusted_Connection=True;TrustServerCertificate=True;";
+
+    public SqlConnection GetConnection()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
+
